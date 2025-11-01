4cf89d068a0a167b100cd3c0ea3064a7f5f4a3ba
EniolaCodes <adebimpeniola@gmail.com>
2025-11-01 09:13:57 +0100

    update the constants folder

---
+++ constants/index.ts
@@ -0,0 +1,24 @@
+export const APP_NAME = "ALX Listing App";
+
+export const API_ENDPOINTS = {
+  LISTINGS: "",
+  BOOKINGS: "",
+  AUTH: "",
+};
+
+export const UI_CONSTANTS = {
+  ANIMATION_DURATION_MS: 200,
+  DEFAULT_BREAKPOINT: 768,
+};
+
+export const PLACEHOLDER_IMAGES = {
+  PROPERTY: "/assets/images/image1.png",
+  AVATAR: "/assets/images/image2.png",
+};
+
+export default {
+  APP_NAME,
+  API_ENDPOINTS,
+  UI_CONSTANTS,
+  PLACEHOLDER_IMAGES,
+};
